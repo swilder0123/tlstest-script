c19433119bd6c99530f4b10c9c4e25f00de8a76f
Steven Wilder <swilder@microsoft.com>
2020-11-04 10:46:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test TLS versioning on Azure storage accounts
+
+This Powershell script allows you to configure the Powershell CLI as a TLS 1.1 and TLS 1.2 client
+to test connectivity to Azure storage. 
---
+++ test-storagesecurity.ps1
@@ -0,0 +1,52 @@
+# note: these commands were tested via cut-and-paste into the Powershell console
+# test connectivity from downlevel for storage connectivity
+
+# -------SNIP-------------
+
+# Set the TLS version used by the PowerShell client to TLS 1.1.
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls11;
+
+# Set the accountname below to an Azure Blob Storage account, configured with a minimum TLS level of 1.2
+$accountName = "azblobacct"
+$rgName = "azblobacct-rg"
+
+# Attempt to create a new container - THIS SHOULD FAIL
+write-host "Attempting to create a new container via TLS 1.1"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName -Name $accountName
+$ctx = $storageAccount.Context
+New-AzStorageContainer -Name "sample-container" -Context $ctx
+
+# Set the accountname below to an Azure Data Lake Storage account, configured with a minimum TLS level of 1.2
+$accountName = "azuredlsacct"
+$rgName = "azuredlsacct-rg"
+
+# Attempt to create a new folder in a ADLS - THIS SHOULD FAIL
+write-host "Attempting to create a new folder via TLS 1.1"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName -Name $accountName
+$ctx = $storageAccount.Context
+New-AzStorageContainer -Name "sample-container" -Context $ctx
+
+# --------SNIP------------
+
+# Set the TLS version used by the PowerShell client to TLS 1.2.
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+# Set the accountname below to an Azure Blob Storage account, configured with a minimum TLS level of 1.2
+$accountName = "azblobacct"
+$rgName = "azblobacct-rg"
+
+# Attempt to create a new container - THIS SHOULD SUCCEED
+write-host "Attempting to create a new container via TLS 1.2"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName -Name $accountName
+$ctx = $storageAccount.Context
+New-AzStorageContainer -Name "sample-container" -Context $ctx
+
+# Set the accountname below to an Azure Data Lake Storage account, configured with a minimum TLS level of 1.2
+$accountName = "azuredlsacct"
+$rgName = "azuredlsacct-rg"
+
+# Attempt to create a new folder in a ADLS - THIS SHOULD SUCCEED
+write-host "Attempting to create a new folder via TLS 1.2"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName -Name $accountName
+$ctx = $storageAccount.Context
+New-AzStorageContainer -Name "sample-container" -Context $ctx
